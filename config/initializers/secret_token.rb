7eb865bbf66dc857cc66486e41c787149bd7a9c4
Dario Seminara <dario@bandofcoders.com>
2011-12-12 09:35:56 -0300

    Rails 3.1.3 application created from scratch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseOpenidTest::Application.config.secret_token = 'd03dc5d5cc69ef61e376bdafe5552ee2b9a5dd3efe72036ddb393d18686590898b1e238ef61fac91ad5c23b77c8e3058e9e16010f06e0bc3b6dbc4ac20d01a03'
